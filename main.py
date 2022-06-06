b1b4a54b915f686fb9a0242646312a73af49e374
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-06-05 23:45:07 -0500

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 on = True
@@ -17,6 +17,5 @@ while on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink):
-            coins = input("Insert coins.")
-            if money_machine.make_payment(coins):
+            if money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
